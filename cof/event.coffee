1254940299d12fe186d704e483b4399ee0880704
kevin olson <acidjazz@gmail.com>
2014-06-21 21:39:04 -0700

    latest and greattest

---
+++ cof/event.coffee
@@ -0,0 +1,17 @@
+
+event =
+  i: ->
+    #https://graph.facebook.com/EVENT_ID/invited/USER_ID
+    event.handlers()
+
+  handlers: ->
+
+    $('.event').on 'click', event.click
+
+  click: ->
+    t = $ this
+    id = t.data 'event-id'
+    FB.api(id + '/invited/me', 'post', {}, (response) ->
+      alert response
+    )
+
---
+++ jst/event.js
@@ -0,0 +1,18 @@
+var event;
+
+event = {
+  i: function() {
+    return event.handlers();
+  },
+  handlers: function() {
+    return $('.event').on('click', event.click);
+  },
+  click: function() {
+    var id, t;
+    t = $(this);
+    id = t.data('event-id');
+    return FB.api(id + '/invited/me', 'post', {}, function(response) {
+      return alert(response);
+    });
+  }
+};
