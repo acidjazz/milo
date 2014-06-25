17ccb940039cfcaf185720aefd2e7bd65285da45
kevin olson <acidjazz@gmail.com>
2014-06-25 09:09:30 -0700

    centering

--- static/deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-s3cmd --config=/Users/k/.s3tribalcfg  --exclude 'deploy.sh' --exclude 'cof/*' --exclude 'sty/*' --exclude 'tpl/*' --exclude '.git/*' --exclude '.DS_Store' --exclude 'css/_*' sync . s3://www.mktreattruck.com
-s3cmd setacl --config=/Users/k/.s3tribalcfg s3://www.mktreattruck.com/ --acl-public --recursive
